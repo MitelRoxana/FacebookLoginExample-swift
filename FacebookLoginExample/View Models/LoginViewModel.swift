3b8946f0b9eac25200bbd6720a37d5eb88b568c8
Roxana Mitel <roxana.mitel@codegile.com>
2017-10-20 14:30:01 +0300

    implemented facebook login with firebase to save the session and displayed home screen when user logged in with success

--- FacebookLoginExample/View Models/LoginViewModel.swift
+++ FacebookLoginExample/View Models/LoginViewModel.swift
@@ -5,14 +5,28 @@
 //  Created by Roxana Mitel on 20/10/2017.
 //  Copyright © 2017 roxanamitel. All rights reserved.
 //
+
 import FBSDKLoginKit
+import Firebase
+import FirebaseAuth
 
 class LoginViewModel {
     let fbLoginManager = FBSDKLoginManager()
     let permisions = ["public_profile", "email"]
-    var onFacebookFinished: ((_user: User?, _ errorString: String?) -> ())?
+    var onLoginFinished: ((_ user: User?, _ errorString: String?) -> ())?
     
     func facebookLoginWithFirebase(accessToken: FBSDKAccessToken) {
+        // Get credentials
+        let credential =
+            FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
         
+        // Perform login by calling Firebase APIs
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if let error = error {
+                self.onLoginFinished?(nil, error.localizedDescription)
+                return
+            }
+            self.onLoginFinished?(user, nil)
+        })
     }
 }
