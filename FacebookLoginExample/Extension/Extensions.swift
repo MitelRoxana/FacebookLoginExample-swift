dab3e03beb575f95e5abcfe393a1fe5021b2aa82
Roxana Mitel <roxana.mitel@codegile.com>
2017-10-20 13:35:32 +0300

    created UIView extension, this helps us to customize views in storyboard.

---
+++ FacebookLoginExample/Extension/Extensions.swift
@@ -0,0 +1,69 @@
+//
+//  Extension.swift
+//  FacebookLoginExample
+//
+//  Created by Roxana Mitel on 20/10/2017.
+//  Copyright © 2017 roxanamitel. All rights reserved.
+//
+
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable var shadow: Bool {
+        get {
+            return layer.shadowOpacity > 0.0
+        }
+        set {
+            if newValue == true {
+                self.addShadow()
+            }
+        }
+    }
+    
+    func addShadow(shadowColor: CGColor = UIColor.black.cgColor,
+                   shadowOffset: CGSize = CGSize(width: 0, height: 0),
+                   shadowOpacity: Float = 0.4,
+                   shadowRadius: CGFloat = 6.0) {
+        layer.shadowColor = shadowColor
+        layer.shadowOffset = shadowOffset
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowRadius = shadowRadius
+    }
+}
